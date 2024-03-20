ae4c708f5e415cbf6744811999ed574d6fe2cada
oliveeiralucas <lucas.oliveira@edu.unifil.br>
2024-03-20 09:41:52 -0300

    chore: npm upgrade -> atualiza pacotes para as versões de 2024/03

---
+++ src/Main.tsx
@@ -0,0 +1,10 @@
+import 'Main.css'
+
+import { createRoot } from 'react-dom/client'
+
+import App from '@/components/App'
+
+const container = document.getElementById('root') as HTMLDivElement
+const root = createRoot(container)
+
+root.render(<App />)
---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div className="flex justify-center bg-gray-300">
+      <p>Hello, world</p>
+    </div>
+  )
+}
+
+export default App
